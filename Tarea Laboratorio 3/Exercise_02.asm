0e7ba5abbb63a921ad2526c41cb83105e9febe23
Rocio Landaverde <00153118@uca.edu.sv>
2021-05-11 00:21:19 -0700

    [Modify] E02: Finish

---
+++ Tarea Laboratorio 3/Exercise_02.asm
@@ -0,0 +1,18 @@
+        org 100h
+
+section .text
+
+        XOR AX, AX ;Limpiando el registro AX
+        MOV AL, 1d ; Inicializo el factorial
+        MOV SI, 1d ; Inicializo el contador
+        JMP factorial
+factorial:
+        CMP SI,5d
+        JA finalizar
+        MUL SI
+        ADD SI,1d
+        JMP factorial
+
+finalizar:
+        MOV [20Bh],AL 
+        int 20h
